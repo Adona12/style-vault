99071d4bcf29e6299244a9405c10164a9c932534
Adona12 <adonatesfaye99@gmail.com>
2023-04-24 14:47:26 +0300

    add new library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
+  const categoreies = [
+    {
+      id: 1,
+      title: "Hats",
+      subtitle: "Shop Now",
+      img: "",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      subtitle: "Shop Now",
+      img: "",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      subtitle: "Shop Now",
+      img: "",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      subtitle: "Shop Now",
+      img: "",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      subtitle: "Shop Now",
+      img: "",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="flex flex-row">
+        {categoreies.map((category) => {
+          return (
+            <div className="categories basis-1/3">
+              <div className="category">
+                {category.title}
+                <p>Shop Now</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
